da5f7eb4597a6971c435fa1b3cd40b57db525fa4
Henry Walter <airstrikeminecraft@gmail.com>
2025-04-07 02:29:38 +0100

    Potential discord crash fix and mitigate  CVE-2024-27303

--- app/assets/js/discordwrapper.js
+++ app/assets/js/discordwrapper.js
@@ -1,52 +1,65 @@
 // Work in progress
-const { LoggerUtil } = require('helios-core')
+const {LoggerUtil} = require('helios-core')
 
 const logger = LoggerUtil.getLogger('DiscordWrapper')
 
-const { Client } = require('discord-rpc-patch')
+const {Client} = require('discord-rpc-patch')
 
 const Lang = require('./langloader')
 
 let client
 let activity
 
-exports.initRPC = function(genSettings, servSettings, initialDetails = Lang.queryJS('discord.waiting')){
-    client = new Client({ transport: 'ipc' })
+exports.initRPC = function (genSettings, servSettings, initialDetails = Lang.queryJS('discord.waiting')) {
+    try {
+        client = new Client({transport: 'ipc'})
 
-    activity = {
-        details: initialDetails,
-        state: Lang.queryJS('discord.state', {shortId: servSettings.shortId}),
-        largeImageKey: servSettings.largeImageKey,
-        largeImageText: servSettings.largeImageText,
-        smallImageKey: genSettings.smallImageKey,
-        smallImageText: genSettings.smallImageText,
-        startTimestamp: new Date().getTime(),
-        instance: false
-    }
-
-    client.on('ready', () => {
-        logger.info('Discord RPC Connected')
-        client.setActivity(activity)
-    })
-    
-    client.login({clientId: genSettings.clientId}).catch(error => {
-        if(error.message.includes('ENOENT')) {
-            logger.info('Unable to initialize Discord Rich Presence, no client detected.')
-        } else {
-            logger.info('Unable to initialize Discord Rich Presence: ' + error.message, error)
+        activity = {
+            details: initialDetails,
+            state: Lang.queryJS('discord.state', {shortId: servSettings.shortId}),
+            largeImageKey: servSettings.largeImageKey,
+            largeImageText: servSettings.largeImageText,
+            smallImageKey: genSettings.smallImageKey,
+            smallImageText: genSettings.smallImageText,
+            startTimestamp: new Date().getTime(),
+            instance: false
         }
-    })
+
+        client.on('ready', () => {
+            logger.info('Discord RPC Connected')
+            client.setActivity(activity)
+        })
+
+        client.login({clientId: genSettings.clientId}).catch(error => {
+            if (error.message.includes('ENOENT')) {
+                logger.info('Unable to initialize Discord Rich Presence, no client detected.')
+            } else {
+                logger.info('Unable to initialize Discord Rich Presence: ' + error.message, error)
+            }
+        })
+    } catch (e) {
+        logger.error('Error initializing Discord RPC: ' + e.message, e)
+    }
 }
 
-exports.updateDetails = function(details){
-    activity.details = details
-    client.setActivity(activity)
+exports.updateDetails = function (details) {
+
+    try {
+        activity.details = details
+        client.setActivity(activity)
+    } catch (e) {
+        logger.error('Error updating Discord RPC details: ' + e.message, e)
+    }
 }
 
-exports.shutdownRPC = function(){
-    if(!client) return
-    client.clearActivity()
-    client.destroy()
-    client = null
-    activity = null
+exports.shutdownRPC = function () {
+    try {
+        if (!client) return
+        client.clearActivity()
+        client.destroy()
+        client = null
+        activity = null
+    } catch (e) {
+        logger.error('Error shutting down Discord RPC: ' + e.message, e)
+    }
 }
